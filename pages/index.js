02b07f29c1018338ecfd602147298badd4ae714f
blairchapman632-glitch <blairchapman632@gmail.com>
2025-09-14 14:59:53 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">
-        Shop Task Tracker App
+        Byford Pharmacy Chalkboard
       </h1>
     </div>
   );
