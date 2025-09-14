374c058330e79794749120bf6f01fdfa22c38b70
blairchapman632-glitch <blairchapman632@gmail.com>
2025-09-14 14:31:35 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Shop Task Tracker App
+      </h1>
+    </div>
+  );
+}
