628059237fbf4a0ccc3516f6b6e531102a282ebe
blairchapman632-glitch <blairchapman632@gmail.com>
2025-09-29 23:55:04 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -275,7 +275,7 @@ export default function HomePage() {
   </div>
 
   {/* Activity (fills rest of column, scrolls) */}
-  <div className="border rounded-xl p-3 bg-white min-h-[300px] overflow-y-auto nice-scroll">
+  <div className="border rounded-xl p-3 bg-white min-h-[240px] overflow-y-auto nice-scroll">
 
 
 
